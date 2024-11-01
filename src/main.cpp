a8469a1f979e18dc3e3e5bbe334cc0cd9a291c79
FarelW <eeksapi321@gmail.com>
2024-11-01 23:46:02 +0700

    feat: Create Folder Structure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "utils/Foo.cpp" 
+#include "utils/Bar.cpp"
+#include "utils/Cube.cpp"
+
+int main() {
+    Cube cube;
+    cube.randomizeCube();
+    cube.printCube();
+
+    cube.randomSwap();
+    cube.printCube();
+
+    return 0;
+}
---
+++ src/utils/Bar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+class Bar {
+public:
+    void printMessage() {
+        cout << "Hello Bar!" << endl;
+    }
+};
