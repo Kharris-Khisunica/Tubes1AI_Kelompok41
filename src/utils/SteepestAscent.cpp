f19431b9ed47ee314817e29a7bf2c697bd6abd8a
FarelW <eeksapi321@gmail.com>
2024-11-04 00:01:47 +0700

    fix: steepest ascent

--- src/utils/SteepestAscent.cpp
+++ src/utils/SteepestAscent.cpp
@@ -6,12 +6,12 @@ SteepestAscent::SteepestAscent(Cube init) : LocalSearch(init) {}
 void SteepestAscent::generateAllNeighbors() {
     neighborValues.clear();
 
-    for (int x1 = 0; x1 < initialState.getSize(); ++x1) {
-        for (int y1 = 0; y1 < initialState.getSize(); ++y1) {
-            for (int z1 = 0; z1 < initialState.getSize(); ++z1) {
-                for (int x2 = 0; x2 < initialState.getSize(); ++x2) {
-                    for (int y2 = 0; y2 < initialState.getSize(); ++y2) {
-                        for (int z2 = 0; z2 < initialState.getSize(); ++z2) {
+    for (int x1 = 0; x1 < currentState.getSize(); ++x1) {
+        for (int y1 = 0; y1 < currentState.getSize(); ++y1) {
+            for (int z1 = 0; z1 < currentState.getSize(); ++z1) {
+                for (int x2 = 0; x2 < currentState.getSize(); ++x2) {
+                    for (int y2 = 0; y2 < currentState.getSize(); ++y2) {
+                        for (int z2 = 0; z2 < currentState.getSize(); ++z2) {
                             if (x1 != x2 || y1 != y2 || z1 != z2) {
                                 int value1 = currentState.getValue(x1, y1, z1);
                                 int value2 = currentState.getValue(x2, y2, z2);
@@ -41,24 +41,33 @@ void SteepestAscent::Algorithm() {
         foundBetter = false;
         generateAllNeighbors();
 
-        for (const auto &neighborTuple : neighborValues) {
-            Cube neighbor = get<0>(neighborTuple);
-            int neighborObjectiveValue = get<1>(neighborTuple);
-            int swappedValue1 = get<2>(neighborTuple);
-            int swappedValue2 = get<3>(neighborTuple);
+        Cube bestNeighbor;
+        int bestNeighborValue = currentObjectiveValue;
+        int value1 = 0, value2 = 0;
 
-            if (neighborObjectiveValue > currentObjectiveValue) {
-                currentState = neighbor;
-                currentObjectiveValue = neighborObjectiveValue;
+        for (auto &neighborTuple : neighborValues) {
+            Cube neighbor = std::get<0>(neighborTuple);
+            int neighborObjectiveValue = std::get<1>(neighborTuple);
+            int currentValue1 = std::get<2>(neighborTuple);
+            int currentValue2 = std::get<3>(neighborTuple);
+
+            if (neighborObjectiveValue > bestNeighborValue) {
+                bestNeighbor = neighbor;
+                bestNeighborValue = neighborObjectiveValue;
+                value1 = currentValue1;
+                value2 = currentValue2;
                 foundBetter = true;
-
-                addNextStep(currentState, swappedValue1, swappedValue2);
             }
+        }
+
+        if (foundBetter) {
+            currentState = bestNeighbor;
+            currentObjectiveValue = bestNeighborValue;
+            addNextStep(currentState, value1, value2);
         }
     }
 
     finalState = currentState;
-
     if (finalState.objectiveFunction() == 0) {
         found = true;
     }
@@ -66,3 +75,4 @@ void SteepestAscent::Algorithm() {
     auto end = high_resolution_clock::now();
     timeTaken = duration_cast<duration<double>>(end - start).count();
 }
+
